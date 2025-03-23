656c7232931037a31ec2efe5482575072e104b52
Aditya <moodyadi30@gmail.com>
2025-03-23 14:49:51 +0530

    Improved login error

--- src/app/api/logout/route.js
+++ src/app/api/logout/route.js
@@ -3,18 +3,16 @@ import { authOptions } from "../auth/[...nextauth]/options";
 
 export async function POST() {
   const session = await getServerSession(authOptions);
-if (session) {
-  return new Response(JSON.stringify({ success: true }), {
-    status: 200,
-    headers: {
-      "Set-Cookie": [
-        `_Host-next-auth.csrf-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Secure; HttpOnly; SameSite=Lax`,
-        `__Secure-next-auth.session-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Secure; HttpOnly; SameSite=Lax`,
-      ],
-    },
-  });
-}
-
+  if (session) {
+    return new Response(JSON.stringify({ success: true }), {
+      status: 200,
+      headers: {
+        "Set-Cookie": [
+          `_Host-next-auth.csrf-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Secure; HttpOnly; SameSite=Lax`,
+          `__Secure-next-auth.session-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Secure; HttpOnly; SameSite=Lax`,
+        ],
+      },
+    });
   }
 
   return new Response(
