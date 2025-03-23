a542820ecf9673f70f2b156a11f449a30e3bc398
Aditya <moodyadi30@gmail.com>
2025-03-23 11:32:03 +0530

    Changed eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,8 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-page-custom-font": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
