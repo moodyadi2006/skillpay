1d1e83f9e80cb368e14767fceb3afb16db059d15
Aditya <moodyadi30@gmail.com>
2025-03-24 19:52:51 +0530

    Changed email service from render to nodemailer

--- src/helpers/sendVerificationEmails.ts
+++ src/helpers/sendVerificationEmails.ts
@@ -1,6 +1,6 @@
-import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
 import { ApiResponse } from "../types/ApiResponse";
+import transporter from '@/lib/nodemailer';
 
 export async function sendVerificationEmail(
   email: string,
@@ -8,10 +8,10 @@ export async function sendVerificationEmail(
   verifyCode: string
 ): Promise<ApiResponse> {
   try {
-    await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
+    await transporter.sendMail({
+      from: '"Skill Pay" <jude.moen@ethereal.email>', 
       to: [email],
-      subject: "SkillPay | Verification code",
+      subject: "SkillPay | Veriy Code", // Subject line
       react: VerificationEmail({ fullName: fullName, otp: verifyCode }),
     });
     return {
---
+++ src/lib/nodemailer.ts
@@ -0,0 +1,13 @@
+import nodemailer from "nodemailer"; 
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.USER_EMAIL,
+    pass: process.env.USER_PASSWORD,
+  },
+});
+
+export default transporter;
